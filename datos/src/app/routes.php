4cd21c91099aa402eeeeb2b83b42f83e73da168b
STEVEN.ALVARADOGUZMAN <wtfAndres05@gmail.com>
2025-05-06 19:34:17 -0600

    first commit

---
+++ datos/src/app/routes.php
@@ -0,0 +1,22 @@
+<?php
+    namespace App\controllers;
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+
+    use Slim\Routing\RouteCollectorProxy;
+
+    $app->get('/', function (Request $request, Response $response, $args) {
+        $response->getBody()->write("FCB 9 - 2 RVAR");
+        return $response;
+    });
+    
+
+    $app->group('/api',function(RouteCollectorProxy $api){
+        $api->group('/producto',function(RouteCollectorProxy $producto){
+            $producto->get('/read[/{id}]', Artefacto::class . ':read');
+            $producto->post('', Artefacto::class . ':create');
+            $producto->put('/{id}', Artefacto::class . ':update');
+            $producto->delete('/{id}', Artefacto::class . ':delete');
+            $producto->get('/filtrar', Artefacto::class . ':filtrar');
+        });
+    });
